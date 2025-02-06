703535aa6f7289b75f77f3994250651307e80b4b
Akane <710105188@qq.com>
2025-02-06 12:04:41 +0800

    finish A*

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,9 +1,17 @@
 package lab11.graphs;
 
+import java.util.PriorityQueue;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeAStarPath extends MazeExplorer {
+    /* Inherits public fields:
+    public int[] distTo;
+    public int[] edgeTo;
+    public boolean[] marked;
+    */
+
     private int s;
     private int t;
     private boolean targetFound = false;
@@ -14,30 +22,84 @@ public class MazeAStarPath extends MazeExplorer {
         maze = m;
         s = maze.xyTo1D(sourceX, sourceY);
         t = maze.xyTo1D(targetX, targetY);
+        // Initialize all distances in distTo array to Integer.MAX_VALUE
+        for (int i = 0; i < distTo.length; i++) {
+            distTo[i] = Integer.MAX_VALUE;
+        }
+
         distTo[s] = 0;
         edgeTo[s] = s;
     }
 
-    /** Estimate of the distance from v to the target. */
+    /**
+     * Estimate of the distance from v to the target.
+     */
     private int h(int v) {
-        return -1;
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
     }
 
-    /** Finds vertex estimated to be closest to target. */
+    /**
+     * Finds vertex estimated to be closest to target.
+     */
     private int findMinimumUnmarked() {
         return -1;
         /* You do not have to use this method. */
     }
 
-    /** Performs an A star search from vertex s. */
+    private static class Node implements Comparable<Node> {
+        int vertex;
+        int priority;
+
+        Node(int v, int p) {
+            this.vertex = v;
+            this.priority = p;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.priority, other.priority);
+        }
+    }
+
+
+    /**
+     * Performs an A star search from vertex s.
+     */
     private void astar(int s) {
-        // TODO
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Node source = new Node(s, 0);
+        distTo[s] = 0;
+        pq.add(source);
+
+        while (!pq.isEmpty()) {
+            int v = pq.poll().vertex;
+            if (marked[v]) {
+                continue;
+            }
+
+            marked[v] = true;
+            announce();
+
+            if (v == t) {
+                targetFound = true;
+                return;
+            }
+
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    if (distTo[v] + 1 < distTo[w]) {
+                        distTo[w] = distTo[v] + 1;
+                        edgeTo[w] = v;
+                        pq.add(new Node(w, distTo[w] + h(w)));
+                    }
+                }
+            }
+        }
+
     }
 
     @Override
     public void solve() {
         astar(s);
     }
-
 }
-
